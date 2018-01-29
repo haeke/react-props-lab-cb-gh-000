838c59f370b24e45db72ec9e401a44a0023a75b1
edwin rivera <waehake@gmail.com>
2018-01-29 16:03:00 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import Spaceship from './components/Spaceship';
 
 const colors = ['black', 'red'];
-const 
+const hasRockets = false;
 
 ReactDOM.render(
   <Spaceship name="Millennium Falcon" speed={10} />,
