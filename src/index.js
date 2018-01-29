8eea57214e5543c23f6844d1fe31c365bbe3e3d0
edwin rivera <waehake@gmail.com>
2018-01-29 16:02:39 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Spaceship from './components/Spaceship';
 
+const colors = ['black', 'red'];
+
 ReactDOM.render(
   <Spaceship name="Millennium Falcon" speed={10} />,
   document.getElementById('global')
